758e923286ce872519b950f8ada689b4877c12d5
luchini18 <luchini18@up.edu>
2018-06-28 14:25:19 -0600

    Added more tests for inputs, graphing distances is now possible, can now use 'p' arg to print data and 'g' arg to not graph data, commenting

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ contains the Cambridge Crystallographic Data Centre library (CCDC). The program
 may take in in a SMILES string along with additional optional arguments that 
 can measure the distance between two atoms, an angle between three atoms, or a 
 torsion angle between four atoms by searching the Cambridge Structural
-Database of crystal structures.
+Database of crystal structures and may be used to graphically compare results.
 If the measurement type and indices of the involved atoms are known, they
 may be included in the command argument. If the indices are not known, the
 types of measurement may still be specified. If no measurements are 
@@ -24,6 +24,8 @@ option to export the data in a .CSV file.
 * t (torsion angle measurement) followed by three atom indices (also optional)
 * s (save data) exports data as a c2m file 
 * lim (search hit limit) followed by a number to limit the number of search hits
+* p (print) prints the data to the command line
+* g (graph) turns graphing of two measurements off (default behavior is on)
 	
 ## Sample Arguments
 Measures two torsion angles with specified indices:
@@ -41,14 +43,11 @@ CCO d 0 1 a 0 1 2
 Removes a search limit and exports the resulting structural data:
 ```
 'C1=CC2=NC1=CC3=NC(=CC4=NC(=CC5=NC(=C2)C=C5)C=C4)C=C3' lim 0 s 
-
 ```
   
 ## Known Bugs
 * Need to find out how to access indices of hydrogen atoms given a SMILES string
-* Need to add error checking for indices/smiles inputs
-* UserWarning occurs when generating the graph since 'normed' kwarg is depricated.
-* Need to work on making the plot more versatile graphing distances/angles, currently works best for comparing torsion angles.
+* UserWarning occurs when generating the graph since 'normed' kwarg is depricated
 	
 ## Other Notes
 * If a measurement is not specified or indices of a measurement are not given,
--- structure_search.py
+++ structure_search.py
@@ -9,8 +9,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 from ccdc.search import SMARTSSubstructure, SubstructureSearch 
-from ccdc.io import csd_version
-
+from ccdc import io
 
 #for testing
 search = True 			#True: the program will perform a search on the input SMILES
@@ -23,82 +22,115 @@ maxHits = 1000			#Max number of hits until the search ends, default at 1000
 #number of measurements for naming purposes d, a, t respectively
 measurement = np.zeros(shape=(3,2))
 
+#for adding a distance measurement
 def addDist(atom = None, askIndex = False):
 	global substructure, substructure_search, sub_id, measurement
 	name = 'D'+str(int(measurement[0][1]))
-	
-	#the atom indices have been specified, add a search element 
 	if not askIndex: 
 		substructure_search.add_distance_measurement(name,
 			sub_id, atom[0], sub_id, atom[1])
-	#obtain indices for distance	
 	else: 
-		#print molecule to user so they can see indices
 		for i in substructure.atoms:
 			print(i.index, i)
-		#obtain values from user and add measurement search
-		atom_i = input("Enter two indices to measure a distance: ")
-		atom_i = list(map(int, atom_i.split()))
+		while True:
+			try:
+				atom_i = input("Enter two indices to measure a distance (# #): ")
+				atom_i = list(map(int, atom_i.split()))
+			except ValueError:
+				print('Try Again with two numbers')
+				continue
+			if len(atom_i) != 2:
+				print('Try Again with two numbers')
+				continue
+			elif not all(i < len(substructure.atoms) for i in atom_i):
+				print('Enter indices within the range of given atoms')
+				continue
+			elif not sorted(atom_i) == list(sorted(set(atom_i))):
+				print('Enter two different numbers')
+				continue 
+			else:
+				break
 		substructure_search.add_distance_measurement(name,
 			sub_id, atom_i[0], sub_id, atom_i[1])
-	
-	print('Distance', name, "added to the search.")
+	print('Distance', name, 'added to the search.')
 	measurement[0][1] += 1
-				
+
+#for adding an angle measurement		
 def addAngle(atom = None, askIndex = False):
 	global substructure, substructure_search, sub_id, measurement
 	name = 'A'+str(int(measurement[1][1]))
-	
-	#the atom indices have been specified, add a search element 
 	if not askIndex: 
 		substructure_search.add_angle_measurement(name,
 			sub_id, atom[0], sub_id, atom[1], sub_id, atom[2])
-	#obtain indices for angle	
 	else: 
-		#print molecule to user so they can see indices
 		for i in substructure.atoms:
 			print(i.index, i)
-		#obtain values from user and add measurement search
-		atom_i = input("Enter three indices to measure an angle: ")
-		atom_i = list(map(int, atom_i.split()))
+		while True:
+			try:
+				atom_i = input("Enter three indices to measure a distance (# # #): ")
+				atom_i = list(map(int, atom_i.split()))
+			except ValueError:
+				print('Try Again with three numbers')
+				continue
+			if len(atom_i) != 3:
+				print('Try Again with three numbers')
+				continue
+			elif not all(i < len(substructure.atoms) for i in atom_i):
+				print('Enter indices within the range of given atoms')
+				continue
+			elif not sorted(atom_i) == list(sorted(set(atom_i))):
+				print('Enter three different numbers')
+				continue 
+			else:
+				break
 		substructure_search.add_angle_measurement(name,
-			sub_id, atom_i[0], sub_id, atom_i[1], sub_id, atom_i[2])
-	
+			sub_id, atom_i[0], sub_id, atom_i[1], sub_id, atom_i[2])	
 	print('Angle', name, "added to the search.")		
 	measurement[1][1] += 1
-		
+
+#for adding a torsion angle measurement		
 def addTor(atom = None, askIndex = False):
 	global substructure, substructure_search, sub_id, measurement
 	name = 'TOR'+str(int(measurement[2][1]))
-	
-	#the atom indices have been specified, add a search element 
 	if not askIndex: 
 		substructure_search.add_torsion_angle_measurement(name,
 			sub_id, atom[0], sub_id, atom[1], sub_id, atom[2], sub_id, atom[3])
-	#obtain indices for angle
 	else: 
-		#print molecule to user so they can see indices
 		for i in substructure.atoms:
 			print(i.index, i)
-		#obtain values from user and add measurement search
-		atom_i = input("Enter four indices to measure a dihedral: ")
-		atom_i = list(map(int, atom_i.split()))
+		while True:
+			try:
+				atom_i = input("Enter four indices to measure a distance (# # # #): ")
+				atom_i = list(map(int, atom_i.split()))
+			except ValueError:
+				print('Try Again with four numbers')
+				continue
+			if len(atom_i) != 4:
+				print('Try Again with four numbers')
+				continue
+			elif not all(i < len(substructure.atoms) for i in atom_i):
+				print('Enter indices within the range of given atoms')
+				continue
+			elif not sorted(atom_i) == list(sorted(set(atom_i))):
+				print('Enter four different numbers')
+				continue 
+			else:
+				break
 		substructure_search.add_torsion_angle_measurement(name,
 			sub_id, atom_i[0], sub_id, atom_i[1], sub_id, atom_i[2],sub_id, atom_i[3])
 	
 	print('Torsion', name, "added to the search.")			
 	measurement[2][1] += 1
 
-def checkMeasurement(arg):
+#parse out user given arguments
+def parseArgs(arg):
 	global graph, printData, maxHits, saveData
 	#check for distance measurement
 	if 'd' in arg:
 		dIndices = [j for j, k in enumerate(arg) if k == 'd']
-		#keep track of how many specified distances we want to measure
 		measurement[0][0] = len(dIndices)
 		for j in np.arange(len(dIndices)):
 			atom = []
-			#check if user specified atom indices in argument
 			try:
 				for k in np.arange(1,3):
 					atom.append(int(arg[dIndices[j]+k]))
@@ -107,17 +139,13 @@ def checkMeasurement(arg):
 				else: 
 					addDist(askIndex=True)
 			except (ValueError, IndexError):
-				addDist(askIndex=True)
-			
-					
+				addDist(askIndex=True)		
 	#check for angle measurements
 	if 'a' in arg:
 		aIndices = [j for j, k in enumerate(arg) if k == 'a']
-		#keep track of how many specified angles we want to measure
 		measurement[1][0] = len(aIndices)
 		for j in np.arange(len(aIndices)):
 			atom = []
-			#check if user specified atom indices in argument
 			try:
 				for k in np.arange(1,4):
 					atom.append(int(arg[aIndices[j]+k]))
@@ -126,17 +154,13 @@ def checkMeasurement(arg):
 				else: 
 					addAngle(askIndex=True)
 			except (ValueError, IndexError):
-				addAngle(askIndex=True)
-						
-						
+				addAngle(askIndex=True)							
 	#check for torsion angle measurement
 	if 't' in arg:
 		tIndices = [j for j, k in enumerate(arg) if k == 't']
-		#keep track of how many specified torsion angles we want to measure
 		measurement[2][0] = len(tIndices)
 		for j in np.arange(len(tIndices)):
 			atom = []
-			#check if user specified atom indices in argument
 			try:
 				for k in np.arange(1,5):
 					atom.append(int(arg[tIndices[j]+k]))
@@ -146,7 +170,7 @@ def checkMeasurement(arg):
 					addTor(askIndex=True)
 			except (ValueError, IndexError):
 				addTor(askIndex=True)
-	
+	#check if search limit was specified
 	if 'lim' in arg:
 		limIndex = arg.index('lim')
 		lim= None
@@ -158,17 +182,21 @@ def checkMeasurement(arg):
 				lim = int(input("Enter a number for max number of hits for the search: "))
 				maxHits = lim
 		except (ValueError, IndexError):
-			#ask for lim
 			lim = int(input("Enter a number for max number of hits for the search: "))
 			maxHits = lim
-			
+	#check if user wants to save data		
 	if 's' in arg:
 		saveData = True
+	#check if user wants to print data to terminal
+	if 'p' in arg:
+		printData = True
+	#check if user does not want to display a graph
+	if 'g' in arg:
+		graph = False
 			
 #if the user didn't pass any arguments, ask for a SMILES string
 if len(sys.argv) == 1:
 	smiles = input("Enter a SMILES string: ")
-	#if user put quotations, remove them
 	if smiles[0] == ("'" or '"'):
 		smiles = smiles[1:]
 	if smiles[-1] == ("'" or '"'):
@@ -176,37 +204,45 @@ if len(sys.argv) == 1:
 else:
 	smiles = sys.argv[1]
 	
-#Prepare a search
-substructure = SMARTSSubstructure(smiles)
+#Prepare a search, checking for invalid submissions
+while True:	
+	try:
+		substructure = SMARTSSubstructure(smiles)
+		if smiles[0] == ("'" or '"'):
+			smiles = smiles[1:]
+		if smiles[-1] == ("'" or '"'):
+			smiles = smiles[:-1]
+	except Exception as e:
+		print(e)
+		smiles = input('Enter another SMILES string:')
+		continue
+	else:
+		break
 substructure_search = SubstructureSearch()
 sub_id = substructure_search.add_substructure(substructure)
 
-#determine if the user has specified other measurements
+#determine if the user has specified other measurements to keep track of
 if len(sys.argv) > 2:
 	arguments = sys.argv
 	del arguments[0:2]
-	checkMeasurement(arguments)
-	#lim or save specified but measurements arent 
+	parseArgs(arguments)
 	if ('d' and 'a' and 't') not in sys.argv:
-		#check if the user wants to search for measurements
 		ans = input('Search for any specific measurements on this molecule? (y/n): ')
 		if ans == 'y':
-			#ask which one, run method
 			ans = input('Which measurements? \nEnter d for distance, a for angle, t for torsion angle: ')
 			ans = list(map(str, ans.split()))
-			checkMeasurement(ans)
+			parseArgs(ans)
 		else: 
 			#user has no measurement parameters, dont graph anything and print the IDs
 			graph = False
 			printData = True
 else:
-	#check if the user wants to search for measurements
 	ans = input('Search for any specific measurements on this molecule? (y/n): ')
 	if ans == 'y':
 		#ask which one, run method
 		ans = input('Which measurements? \nEnter d for distance, a for angle, t for torsion angle: ')
 		ans = list(map(str, ans.split()))
-		checkMeasurement(ans)
+		parseArgs(ans)
 	else: 
 		#user has no measurement parameters, dont graph anything and print the IDs
 		graph = False
@@ -231,7 +267,7 @@ if len(hits) == 0:
 		print("No substructures found.")
 		exit()
 
-#determine names of columns for the DataFrame
+#format data into lists and place it into a dataframe
 columns = ['Identifier']
 mType = ''
 for i in np.arange(3):
@@ -244,8 +280,6 @@ for i in np.arange(3):
 			mType = 'TOR'
 			
 		columns.append(mType+str(j))
-
-#prepare data to be placed into dataframe
 ids = []
 measureHits = [[] for _ in range(len(columns)-1)]
 for h in hits:
@@ -253,8 +287,6 @@ for h in hits:
 	if len(columns) > 1:
 		for i in np.arange(len(columns)-1):
 			measureHits[i].append(float('%.2f' % h.measurements[columns[i+1]]))
-
-#create a dataframe and put the data into it
 hitData = pd.DataFrame(columns=columns)
 hitData['Identifier'] = ids
 for i in np.arange(len(measureHits)):
@@ -264,67 +296,96 @@ for i in np.arange(len(measureHits)):
 if printData:
 	print(hitData.to_string())
 		
-#print data info to user 		
+#print useful search data info to user 		
 structData = str(len(hitData.Identifier)) + ' matching substructures in '+ str(len(hitData.Identifier.unique())) + ' different molecules.'
 print('Found ' + structData)
 
-#Export data 
+#optionally export data to user
 if saveData:
 	filename = os.getcwd() + '/search_' + datetime.datetime.now().strftime ("%H:%M:%S") + '.CSV'
 	hitData.to_csv(filename)
 	print('file saved to: ' + filename) 
 
-#ask user which data to display 
-if len(hitData.columns) == 3:
-	xData = hitData.columns[1]
-	yData = hitData.columns[2]
-elif len(hitData.columns) > 3:
-	print('Which data would you like to display? (enter two indices, # #):')
-	print('i | Measurement')
-	#only show angular data
-	#decide title of graph
-	for i in np.arange(len(columns[1:])):
-		if columns[i+1][0] != 'D':
-			print(str(i) + " | " + str(columns[i+1]))
-	ans = input()
-	ans = list(map(int, ans.split()))
-	xData = hitData.columns[ans[0]+1]
-	yData = hitData.columns[ans[1]+1]
-	print('Graphing \'' + xData + '\' vs \'' + yData + '\'...')
-elif len(hitData.columns) < 3:
-	graph = False
-
-	
+#optionally graph, asking user which data to display, formatting graph and axes titles
 if graph:
-	if xData[0] == 'T' and yData[0] == 'T':
-		title = 'Dihedral Angle Magnitude Comparison'
-	elif xData[0] == 'A' and yData[0] == 'A':
-		title = 'Angle Magnitude Comparison'
-	elif xData[0] == ('T' or 'A') and yData[0] == ('T' or 'A') :
-		title = 'Dihedral Angle vs. Angle Magnitude Comparison'
-	else: 
-		title = 'Angular Magnitude Comparison'
+	if len(hitData.columns) == 3:
+		xData = hitData.columns[1]
+		yData = hitData.columns[2]
+	elif len(hitData.columns) > 3:
+		print('Which data would you like to display? (enter two indices, # #):')
+		print('i | Measurement')
+		for i in np.arange(len(columns[1:])):
+			print(str(i) + " | " + str(columns[i+1]))
+		while True:
+			try:
+				ans = input()
+				ans = list(map(int, ans.split()))
+			except ValueError:
+				print('Try Again with two numbers')
+				continue
+			if len(ans) != 2:
+				print('Try Again with two numbers')
+				continue
+			elif not all(i < len(columns)-1 for i in ans):
+				print('Enter indices within the range of given measurements')
+				continue
+			elif not sorted(ans) == list(sorted(set(ans))):
+				print('Enter two different numbers')
+				continue 
+			else:
+				break
+		xData = hitData.columns[ans[0]+1]
+		yData = hitData.columns[ans[1]+1]	
+		if xData[0] == 'T' and yData[0] == 'T':
+			title = 'Dihedral Angle Magnitude Comparison'
+			xax = xData + ' / degrees'
+			yax = yData + ' / degrees'
+		elif xData[0] == 'A' and yData[0] == 'A':
+			title = 'Angle Magnitude Comparison'
+			xax = xData + ' / degrees'
+			yax = yData + ' / degrees'
+		elif xData[0] == 'D' and yData[0] == 'D':
+			title = 'Distance Comparison'
+			xax = xData + r' / $\AA$'
+			yax = yData + r' / $\AA$'
+		elif any(i in xData for i in ['A', 'T']) and any(item in yData for item in ['A', 'T']):
+			title = 'Dihedral Angle vs. Angle Magnitude Comparison'
+			xax = xData + ' / degrees'
+			yax = yData + ' / degrees'
+		elif any(i in xData for i in ['D']) and any(item in yData for item in ['A', 'T']):
+			title = 'Distance vs. Angle Magnitude Comparison'
+			xax = xData + r' / $\AA$'
+			yax = yData + ' / degrees'
+		elif any(i in xData for i in ['A', 'T']) and any(item in yData for item in ['D']):
+			title = 'Angle Magnitude Comparison vs Distance'
+			xax = xData + ' / degrees'
+			yax = yData + r' / $\AA$'
+		else: 
+			title = 'Angular Magnitude Comparison'
+			
+		print('Graphing \'' + xData + '\' vs \'' + yData + '\'...')
+	elif len(hitData.columns) < 3:
+		graph = False
+
 	#Joint plot Creator
-		#gridsize: higher = smaller hexagons
-		#joint_kws bins: lower = darker hexagons
-		#marginal_kws bins: lower = thicker histograms
+		#gridsize: larger # = smaller hexagons
+		#marginal_kws bins: smaller # = thicker histograms
 	sns.set(style='white', color_codes=True)
 	g = sns.jointplot(x=xData, y=yData,data=hitData, kind="hex",color='b', gridsize=30,
 		stat_func=None, space=0, ratio=5 ,marginal_kws=dict(bins=50, color='r')) 
-		 #xlim=(-15,195), ylim=(-15,195))
-	g.set_axis_labels(xData + ' / degrees',yData + ' / degrees')
+	g.set_axis_labels(xax,yax)
 	anText = "Data from " + structData # add timestamp/version
 	g.fig.text(0.5, 0.05, anText, horizontalalignment='center', verticalalignment='bottom', fontsize = 10)	
-	versionText = "CDS version " + str(int(csd_version())*0.01) + ". Date: " + datetime.datetime.now().strftime ("%m/%d/%Y")
+	versionText = "CDS version " + str(int(io.csd_version())*0.01) + ". Date: " + datetime.datetime.now().strftime ("%m/%d/%Y")
 	g.fig.text(0.5, 0, versionText, horizontalalignment='center', verticalalignment='bottom', fontsize = 10)	
 	g.fig.suptitle(title)
-	#set these different for t vs a 
+	#add specific ticks for torsion angles
 	if xData[0] == 'T':	
 		g.ax_joint.xaxis.set_major_locator(ticker.MultipleLocator(30))
 	if yData[0] == 'T':	
 		g.ax_joint.yaxis.set_major_locator(ticker.MultipleLocator(30))
 	plt.setp(g.ax_marg_y.patches, color='g')
 	plt.subplots_adjust(left=0.2, right=0.9, top=0.9, bottom=0.2) 
-	cax = g.fig.add_axes([.9, .4, .01, .25])  # x, y, width, height
+	cax = g.fig.add_axes([.9, .4, .01, .25])
 	plt.colorbar(cax=cax, format='%.0f')
 	plt.show()
