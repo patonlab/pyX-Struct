47353ba87844884401024f80a3a32cd38cfe1dbf
Robert Paton <robert.paton@colostate.edu>
2018-12-14 17:48:03 -0700

    edited conda and pypi info

--- setup.py
+++ setup.py
@@ -8,16 +8,16 @@ with io.open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-  name = 'pyX-Struct',
-  packages = ['pyX-Struct'],
-  version = '1.0',
+  name = 'pyxstruct',
+  packages = ['pyxstruct'],
+  version = '1.0.1',
   description = 'Scrape Geometric X-ray Data from the Cambridge Structural Database ',
   long_description=long_description,
   long_description_content_type='text/markdown',
   author = 'Paton Research Group',
   author_email = 'robert.paton@colostate.edu',
   url = 'https://github.com/bobbypaton/pyX-Struct',
-  download_url = 'https://github.com/bobbypaton/pyX-Struct/archive/1.0.zip',
+  download_url = 'https://github.com/bobbypaton/pyX-Struct/archive/v1.0.1.zip',
   keywords = ['x-ray structure', 'CCDC', 'SMILES', 'python'],
   classifiers = [],
   install_requires=["numpy","seaborn","pandas","matplotlib"],
